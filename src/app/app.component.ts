9b73d62c58a840102cbaf32a40c90626126e05fb
landough <orlando_wilson@msn.com>
2017-05-19 11:26:11 -0400

    initial configuration and loading theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  // tslint:disable-next-line:component-selector
+  selector: 'body',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Heroes Wireframe Beta';
 }
