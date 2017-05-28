43b4b578ed050d45b3bf7a17c5621ee48a5cb7d2
landough <orlando_wilson@msn.com>
2017-05-27 23:51:09 -0400

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { Observable } from 'rxjs/Observable';
 import * as fromRoot from './reducers/reducers';
 
@@ -9,14 +10,31 @@ import * as fromSideNavActions from './actions/sidenavs';
   // tslint:disable-next-line:component-selector
   selector: 'body',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('wireframeAnimation', [
+      state('start', style({ transform: 'translateX(0) scale(1)' })),
+      state('end', style({ transform: 'translateX(100%) scale(0)' })),
+      // transition('start <=> end', animate('100ms ease-out')),
+      // transition('end => start', animate('100ms ease-in')),
+      transition('void => start', [style({ transform: 'translateX(-100%)' }), animate(200)]),
+      transition('start => end', animate(100, style({ transform: 'translateX(100%) scale(1)' }))),
+      transition('void => end', [style({ transform: 'translateX(100%) scale(1)' }), animate(100)]),
+      transition('end => start', animate(100, style({ transform: 'translateX(0) scale(1)' })))
+    ])
+  ]
 })
 export class AppComponent {
 
   sideNavs$: Observable<any> = Observable.of<any>();
+  animationState = 'start';
 
   constructor(private store: Store<fromRoot.State>) {
     this.sideNavs$ = this.store.select(state => state.sidenavs);
+  }
+
+  animateToggle() {
+    this.animationState = (this.animationState === 'start' ? 'end' : 'start');
   }
 
   toggleStart() {
