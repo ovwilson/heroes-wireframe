fe3e85577318695ece1a543a4ba66cc7fc5a7c6d
landough <orlando_wilson@msn.com>
2017-06-19 22:25:10 -0400

    setup mongo on local

---
+++ scripts/mongo-local-client.js
@@ -0,0 +1,17 @@
+var mongodb = require('mongodb'),
+    config = require('./../config.json'),
+    db;
+
+mongodb.MongoClient.connect(config.localUri, function (err, db) {
+    if (err) throw err;
+
+    var heroes = db.collection('heroes');
+    heroes.insert(config.seedData, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+
+    db.close(function (err) {
+        if (err) throw err;
+    });
+});
